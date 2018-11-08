4b834fd84889c850a4bf1903f4185b306b13b788
alexpl52 <apolishchuk52@gmail.com>
2018-11-08 14:06:35 +0700

    add get_queue_len test case

--- tests.py
+++ tests.py
@@ -1,27 +1,32 @@
 import unittest
+from unittest.mock import patch
+import moto
 
-from startup import ec2, create_instances
-
+from startup import create_instances
 
 # [Тестируемый метод]_[Сценарий]_[Ожидаемое поведение]
 
-class TestEC2CreateInstances(unittest.TestCase):
+# class TestEC2CreateInstances(unittest.TestCase):
 
-    def tearDown(self):
-        ec2.instances.filter(InstanceIds=self.instance_ids).terminate()
+#     def tearDown(self):
+#         ec2.instances.filter(InstanceIds=self.instance_ids).terminate()
 
-    def test_instances_list_returned(self):
-        instanses = create_instances(count=1)
-        self.instance_ids = [i.id for i in instanses]
-        self.assertTrue(all([i for i in instanses]))
-
-
+#     def test_instances_list_returned(self):
+#         instanses = create_instances(count=1)
+#         self.instance_ids = [i.id for i in instanses]
+#         self.assertTrue(all([i for i in instanses]))
 
 
-class TestCalc(unittest.TestCase):
-    pass
+
+
+# class TestCalc(unittest.TestCase):
+#     pass
 
 
 class TestGetQueueLen(unittest.TestCase):
-    pass
+    @patch('startup.get_queue_len', return_value=100)
+    def test_get_queue_len(self, get_queue_len):
+        queue_len = get_queue_len()
+        self.assertIsNotNone(queue_len)
+        self.assertIsInstance(queue_len, int)
 
