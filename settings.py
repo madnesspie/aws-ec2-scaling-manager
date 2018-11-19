5bbcfc806e46752c92f13f2c4f4652ca48ff9407
alexpl52 <apolishchuk52@gmail.com>
2018-11-20 05:11:04 +0700

    Add settings.py with env var

---
+++ settings.py
@@ -0,0 +1,15 @@
+import easy_env
+
+PAUSE = easy_env.get_int('PAUSE', 45)
+CALC_TIME = easy_env.get_int('CALC_TIME', 15)
+DONE_TIME = easy_env.get_int('DONE_TIME', 300)
+VCPU_COUNT = easy_env.get_int('VCPU_COUNT', 2)
+IMAGE_ID = easy_env.get_str('IMAGE_ID', 'ami-14fb1073')
+INSTANCE_TYPE = easy_env.get_str('INSTANCE_TYPE', 't2.micro')
+
+# PAUSE = os.environ.get('PAUSE')
+# CALC_TIME = os.environ.get('CALC_TIME')
+# DONE_TIME = os.environ.get('DONE_TIME')
+# VCPU_COUNT = os.environ.get('VCPU_COUNT')
+# IMAGE_ID = os.environ.get('IMAGE_ID')
+# INSTANCE_TYPE = os.environ.get('PAUSE')
