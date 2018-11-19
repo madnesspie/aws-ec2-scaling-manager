bab7d80385a6b7f1a0767ab90207fbcd2fd94583
alexpl52 <apolishchuk52@gmail.com>
2018-11-20 02:08:44 +0700

    Add level constant

--- logger.py
+++ logger.py
@@ -5,37 +5,43 @@ logging.basicConfig(
     format='%(asctime)s ~ %(levelname)-10s %(name)-25s %(message)s',
     datefmt='%Y-%m-%d %H:%M', level=logging.DEBUG)  # , filename='*.log')
 
-logging.getLogger('boto3').setLevel(logging.WARNING)
-logging.getLogger('botocore').setLevel(logging.WARNING)
-logging.getLogger('requests').setLevel(logging.WARNING)
-logging.getLogger('urllib3').setLevel(logging.WARNING)
+DEBUG = logging.DEBUG
+INFO = logging.INFO
+WARNING = logging.WARNING
+ERROR = logging.ERROR
+CRITICAL = logging.CRITICAL
 
-logging.addLevelName(logging.DEBUG, '🐛 DEBUG')
-logging.addLevelName(logging.INFO, '📑 INFO')
-logging.addLevelName(logging.WARNING, '🤔 WARNING')
-logging.addLevelName(logging.ERROR, '🚨 ERROR')
-logging.addLevelName(logging.CRITICAL, '💥 CRITICAL')
+logging.getLogger('boto3').setLevel(WARNING)
+logging.getLogger('botocore').setLevel(WARNING)
+logging.getLogger('requests').setLevel(WARNING)
+logging.getLogger('urllib3').setLevel(WARNING)
 
+logging.addLevelName(DEBUG, '🐛 DEBUG')
+logging.addLevelName(INFO, '📑 INFO')
+logging.addLevelName(WARNING, '🤔 WARNING')
+logging.addLevelName(ERROR, '🚨 ERROR')
+logging.addLevelName(CRITICAL, '💥 CRITICAL')
 
 def get_logger(name):
     return logging.getLogger(name)
 
 
-def log(params=True, result=True):
+def log(level=DEBUG, params=True, result=True):
     def wrapped(func):
         logger = logging.getLogger(func.__module__)
-        
+
         @wraps(func)
         def inner_wrapped(*args, **kwargs):
-            log_str = f"{func.__name__}() is called "
+            message = f"{func.__name__}() is called "
             if params:
-                log_str += f"with params {args} and {kwargs} "
-            logger.debug(log_str)
+                message += f"with params {args} and {kwargs} "
+            logger.log(level, message)
 
             result = func(*args, **kwargs)
             if result:
-                logger.debug(f"{func.__name__}() returned {result} ")
+                message = f"{func.__name__}() returned {result} "
+                logger.log(level, message)
+
             return result
-        
         return inner_wrapped
     return wrapped
